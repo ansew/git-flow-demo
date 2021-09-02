8467f9ccd09535d7dabb8995ce45418857a15bd2
snezanar <snezana.rajcic@gmail.com>
2021-09-02 19:53:28 +0200

    footer

---
+++ src/footer.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+
+function Footer(){
+    return(
+        <footer class="page-footer">
+          <div class="footer-copyright">
+            <div class="container">
+            © 2021 Copyright Text          
+            </div>
+          </div>
+        </footer>
+
+    )
+}
+export default Footer
